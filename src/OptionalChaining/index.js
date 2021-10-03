35489d95cf85f0304163fe98e1f51bf9bc33f8a1
Alexandre Santos <amenezes031@gmail.com>
2021-10-03 13:52:45 -0300

    use nullish coalescing to return total stars

--- src/OptionalChaining/index.js
+++ src/OptionalChaining/index.js
@@ -4,16 +4,17 @@ var user = {
     username: "alexandresantosm"
 };
 var getUser = function (user, options) {
-    var _a, _b, _c, _d;
-    var avatarUrl = (_a = options === null || options === void 0 ? void 0 : options.fields) === null || _a === void 0 ? void 0 : _a.avatarUrl;
-    var userId = (_b = options === null || options === void 0 ? void 0 : options.fields) === null || _b === void 0 ? void 0 : _b["user-id"];
-    var stars = (_d = (_c = options === null || options === void 0 ? void 0 : options.fields) === null || _c === void 0 ? void 0 : _c.getStars) === null || _d === void 0 ? void 0 : _d.call(_c, user);
+    var _a, _b, _c, _d, _e, _f;
+    var totalStars = (_b = (_a = options === null || options === void 0 ? void 0 : options.fields) === null || _a === void 0 ? void 0 : _a.totalStars) !== null && _b !== void 0 ? _b : 1;
+    var avatarUrl = (_c = options === null || options === void 0 ? void 0 : options.fields) === null || _c === void 0 ? void 0 : _c.avatarUrl;
+    var userId = (_d = options === null || options === void 0 ? void 0 : options.fields) === null || _d === void 0 ? void 0 : _d["user-id"];
+    var stars = (_f = (_e = options === null || options === void 0 ? void 0 : options.fields) === null || _e === void 0 ? void 0 : _e.getStars) === null || _f === void 0 ? void 0 : _f.call(_e, user);
     var username = user.username;
-    return console.log({ userId: userId, username: username, avatarUrl: avatarUrl, stars: stars });
+    return console.log({ userId: userId, username: username, avatarUrl: avatarUrl, stars: stars, totalStars: totalStars });
 };
-getUser(user);
 getUser(user, {
     fields: {
+        totalStars: 0,
         "user-id": 1,
         avatarUrl: 'https://avatars0.githubusercontent.com/u/0',
         getStars: function (user) { return user.stars; }
--- src/OptionalChaining/index.ts
+++ src/OptionalChaining/index.ts
@@ -16,16 +16,18 @@ const user = {
 };
 
 const getUser = (user: GithubUser, options?: logOptions) => {
+    const totalStars = options?.fields?.totalStars ?? 1;
     const avatarUrl = options?.fields?.avatarUrl;
     const userId = options?.fields?.["user-id"];
     const stars = options?.fields?.getStars?.(user);
     const { username } = user;
 
-    return console.log({ userId, username, avatarUrl, stars });
+    return console.log({ userId, username, avatarUrl, stars, totalStars });
 };
 
 getUser(user, {
     fields: {
+        totalStars: 0,
         "user-id": 1,
         avatarUrl: 'https://avatars0.githubusercontent.com/u/0',
         getStars: (user: GithubUser) => user.stars,
